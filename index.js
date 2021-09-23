fc23e6cfd7a1b8492562d57026e904a1c112a5e9
Tasleemofx <oladepotesleem5@gmail.com>
2021-09-23 11:41:35 +0100

    Build added and port edited

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 let notes = require('./notes')
 
 app.use(express.json())
+app.use(cors())
 
 
 app.get('/', (request,response)=>{
@@ -50,7 +52,7 @@ app.delete('/api/notes/:id', (request, response)=>{
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 })
